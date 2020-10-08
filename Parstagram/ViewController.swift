25b3a82e0a3c3f45b2ff76c0dcb96b9a34372efc
Joel Sejas <69014144+jsejas@users.noreply.github.com>
2020-10-08 16:16:44 -0700

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Joel Sejas on 10/8/20.
+//  Copyright © 2020 jsejas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
